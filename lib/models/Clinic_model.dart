f7338b59efd7178bf00af39b3e18f51dfbb61a6c
PeterOrion <Peterkibet70@gmail.com>
2025-07-28 22:31:58 +0300

    Update Clinic_model.dart

--- lib/models/Clinic_model.dart
+++ lib/models/Clinic_model.dart
@@ -1,1 +1,28 @@
+class Clinic {
+  final String name;
+  final String address;
+  final String price;
+  final double rating;
+  final double latitude;
+  final double longitude;
 
+  Clinic({
+    required this.name,
+    required this.address,
+    required this.price,
+    required this.rating,
+    required this.latitude,
+    required this.longitude,
+  });
+
+  factory Clinic.fromJson(Map<String, dynamic> json) {
+    return Clinic(
+      name: json['name'],
+      address: json['address'],
+      price: json['price'],
+      rating: (json['rating'] as num).toDouble(),
+      latitude: (json['latitude'] as num).toDouble(),
+      longitude: (json['longitude'] as num).toDouble(),
+    );
+  }
+}
